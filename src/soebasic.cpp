4d069d104aba76d3e5aaf2f6f8a252659ca1ff97
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-03-11 21:48:18 +0000

    feat: unfolding boilerplate code

--- src/soebasic.cpp
+++ src/soebasic.cpp
@@ -1,23 +1,29 @@
+/*
+ *  soebasic.cpp - basic implementation of the sieve of erastothenes
+ *  CPA @ M.EIC, 2023
+ *  Authors:
+ *      Miguel Rodrigues <up201906042@edu.fe.up.pt>
+ *      Sérgio Estêvão <up201905680@edu.fe.up.pt>
+ */
 #include <cstdint>
 #include <vector>
 #include "soehelpers.h"
 
-
 static constexpr std::uint64_t N = 1 << 25;
-
 
 int
 main(void)
 {
     cpa::sieve_of_erastothenes::PAPI_helper helper;
-    std::vector<bool> primes(N >> 1);
 
+    std::vector<bool> primes(N >> 1);
     std::uint64_t k = 3;
 
     helper.start();
     const auto start = std::chrono::steady_clock::now();
     
     do {
+
         for (std::uint64_t i = k * k; i < N; i += 2 * k) {
             if (i % k == 0)
                 primes[i >> 1] = true;
@@ -26,7 +32,7 @@ main(void)
         do {
             k += 2;
         } while (k * k <= N && primes[k >> 1]);
-
+        
     } while (k * k <= N);
 
     const auto end = std::chrono::steady_clock::now();
@@ -34,8 +40,6 @@ main(void)
     
     const auto elapsed = end - start;
     cpa::sieve_of_erastothenes::report(helper, elapsed, N);
-
-    
     cpa::sieve_of_erastothenes::print(primes, N, std::cerr);
 
     return 0;
---
+++ src/soemultiples.cpp
@@ -0,0 +1,14 @@
+/*
+ *  soemultiples.cpp - implementation of the sieve of erastothenes w/ lookups
+ *  CPA @ M.EIC, 2023
+ *  Authors:
+ *      Miguel Rodrigues <up201906042@edu.fe.up.pt>
+ *      Sérgio Estêvão <up201905680@edu.fe.up.pt>
+ */
+
+
+int 
+main(void)
+{
+    return 0;
+}
