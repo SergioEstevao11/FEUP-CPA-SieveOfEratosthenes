e27c6a4ab808b71582bea2c573d7adc89a950042
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-03-18 23:54:03 +0000

    feat: reorder statements to make it more simple and readable

--- src/soesegmented.cpp
+++ src/soesegmented.cpp
@@ -23,46 +23,45 @@ main(void)
 {
     cpa::sieve_of_erastothenes::PAPI_helper helper;
 
-    std::uint64_t computed = 1;
-    std::uint64_t k = 3, it = 3;
-
-    constexpr std::uint64_t n_sqrt = static_cast<std::uint64_t>(std::sqrt(N));
-    constexpr std::uint64_t segment_size = std::max(n_sqrt, L1D_CACHE_SIZE);
+    constexpr auto n_sqrt = static_cast<std::uint64_t>(std::sqrt(N));
+    constexpr auto segment_size = std::max(n_sqrt, L1D_CACHE_SIZE);
 
     std::bitset<segment_size> sieve;
+
     std::vector<std::pair<std::uint64_t, std::uint64_t>> sieving_primes;
     std::bitset<(n_sqrt >> 1) + 1> is_prime;
-
     is_prime.set();
+
+    std::uint64_t computed = 0, k = 3;
 
     helper.start();
     const auto start = std::chrono::steady_clock::now();
 
     for (std::uint64_t low = 0; low <= N; low += segment_size) {
 
-        const std::uint64_t high = std::min(N, low + segment_size - 1);
+        const auto high = std::min(N, low + segment_size - 1);
 
         for (; k * k <= high; k += 2) {
-            if (is_prime[k >> 1]) {
-                for (std::uint64_t i = k * k; i <= n_sqrt; i += 2 * k) {
-                    is_prime[i >> 1] = false;
-                }
-                sieving_primes.emplace_back(k, k * k - low);
+            if (!is_prime[k >> 1])
+                continue;
+            
+            for (std::uint64_t i = k * k; i <= n_sqrt; i += 2 * k) {
+                is_prime[i >> 1] = false;
             }
+            sieving_primes.emplace_back(k, k * k - low);
         }
 
         sieve.set();
 
         for (auto& [prime, multiple] : sieving_primes) {
-            std::uint64_t i = multiple;
-            for (std::uint64_t j = prime * 2; i < segment_size; i += j) {
-                sieve[i] = false;
+            for (std::uint64_t i = prime * 2; multiple < segment_size; multiple += i) {
+                sieve[multiple] = false;
             }
-            multiple = i - segment_size;
+            multiple -= segment_size;
         }
 
-        for (; it <= high; it += 2) {
-            computed += sieve[it - low];
+        for (auto i = low + 1; i <= high; i += 2) {
+            computed += sieve[i - low];
         }
     }
 
